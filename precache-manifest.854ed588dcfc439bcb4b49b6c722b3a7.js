ec84150c93f9e5d9375f81412f805f2d1765064b
sayantanHack <sayantandatta@gmail.com>
2021-02-05 12:24:04 +0530

    Updates

---
+++ precache-manifest.854ed588dcfc439bcb4b49b6c722b3a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a925a2dac09dfdc5b771e7435c7ddfe",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "0a85e03f64d040dd52f8",
+    "url": "/test-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "936e6d5333e46c61fbbc",
+    "url": "/test-app/static/js/2.b4bb7e49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-app/static/js/2.b4bb7e49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a85e03f64d040dd52f8",
+    "url": "/test-app/static/js/main.e5692773.chunk.js"
+  },
+  {
+    "revision": "d2861ff09a1b52bdd456",
+    "url": "/test-app/static/js/runtime-main.a1b17814.js"
+  }
+]);
