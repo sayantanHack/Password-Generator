f675cd92b37e24295d3220fdf475877dbd5890e8
Sayantan <sayantandatta97@gmail.com>
2024-05-27 16:30:39 +0530

    Update App.js
    Updated the code with button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,86 @@
-import React from 'react';
-//import logo from './public/logo.png';
-import './App.css';
+import { useState, useCallback } from 'react'
+//import reactLogo from "./assets/react.svg";
+//import viteLogo from "/vite.svg";
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello test site!!</h1>
-	  <p>New line added should be deployed :)</p>
-    </div>
-  );
+	const [length, setLength] = useState(8)
+	const [num, setNum] = useState(false)
+	const [char, setChar] = useState(false)
+	const [pswd, setPswd] = useState(1909)
+
+	// creating useCallback update the function using memoisation . it uses function and aray of dependency
+	//format useCallback(function , [d1, d2, d3])
+
+	const passGen = useCallback(() => {
+		var charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		var retVal = ""; 
+
+		if (num) charset += '0123456789'
+		if (char) charset += '!@#$%^&*()_+=-?.><|'
+
+		for (var i = 0, n = charset.length; i < length; ++i) {
+			retVal += charset.charAt(Math.floor(Math.random() * n))
+		}
+		//return retVal;
+		setPswd(retVal)
+	}, [length, num, char, setPswd])
+
+	return (
+		<>
+			<div>
+				<h2>Pasword Generator</h2>
+				<h4>Password length : {length}</h4>
+			</div>
+
+			<div>
+				<input
+					type="text"
+					value={pswd}
+					placeholder="Password"
+					className='psw'
+					//ref={passwordRef}
+				/>
+				<br/>
+				<button onClick={()=>{passGen()}}>Generate</button>
+			</div>
+
+			<div>
+				<input
+					type="range"
+					min={6}
+					max={90}
+					value={length}
+					onChange={(e) => {
+						setLength(e.target.value)
+					}}
+				/>
+				<label> Length: {length}</label>
+			</div>
+			<div>
+				<input
+					type="checkbox"
+					defaultChecked={num}
+					value={length}
+					onChange={(e) => {
+						setNum((prevnum) => !prevnum)
+					}} // checking previous setting check or uncheck is not here
+				/>
+				<label> Numbers </label>
+
+				<input
+					type="checkbox"
+					defaultChecked={num}
+					value={length}
+					onChange={(e) => {
+						setChar((prevchar) => !prevchar)
+					}} // checking previous setting check or uncheck is not here
+				/>
+				<label> Characters </label>
+			</div>
+			<footer className='ft'>powered by Sayantan</footer>
+		</>
+	)
 }
 
-export default App;
+export default App
